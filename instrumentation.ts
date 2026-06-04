a2429bd3dc33ae92042916e0788f80ebcb9c607d
amir <amirrezazadeh.job@gmail.com>
2026-06-04 21:25:52 +0330

    fix instrumentation file

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,5 +1,7 @@
 import { setGlobalDispatcher, ProxyAgent } from "undici";
 
 export async function register() {
-  setGlobalDispatcher(new ProxyAgent("http://127.0.0.1:10808"));
+  if (process.env.HTTP_PROXY) {
+    setGlobalDispatcher(new ProxyAgent(process.env.HTTP_PROXY));
+  }
 }
